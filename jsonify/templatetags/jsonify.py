6c37f0d9301e695c28fe947d282827f5cc3d12eb
Marius Grigaitis <Marius Grigaitis@localhost>
2011-08-24 15:03:03 +0300

    updated to use django encoder

--- jsonify/templatetags/jsonify.py
+++ jsonify/templatetags/jsonify.py
@@ -1,13 +1,14 @@
 from django.core.serializers import serialize
+from django.core.serializers.json import DjangoJSONEncoder
 from django.db.models.query import QuerySet
 from django.utils import simplejson
+from django.utils.safestring import mark_safe
 from django.template import Library
 
 register = Library()
 
+@register.filter
 def jsonify(obj):
     if isinstance(obj, QuerySet):
-        return serialize('json', obj)
-    return simplejson.dumps(obj)
-
-register.filter('jsonify', jsonify)
+        return mark_safe(serialize('json', obj))
+    return mark_safe(simplejson.dumps(obj, cls=DjangoJSONEncoder))
