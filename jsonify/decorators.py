35db32d1bac961d964df4b4b55ee455869b409bb
Marius Grigaitis <Marius Grigaitis@localhost>
2011-08-24 16:30:50 +0300

    added wraps decorator

--- jsonify/decorators.py
+++ jsonify/decorators.py
@@ -1,6 +1,19 @@
 from django.http import HttpResponse
 from django.core.serializers.json import DjangoJSONEncoder
 
+try:
+    from functools import wraps
+except ImportError: 
+    def wraps(wrapped, assigned=('__module__', '__name__', '__doc__'),
+              updated=('__dict__',)):
+        def inner(wrapper):
+            for attr in assigned:
+                setattr(wrapper, attr, getattr(wrapped, attr))
+            for attr in updated:
+                getattr(wrapper, attr).update(getattr(wrapped, attr, {}))
+            return wrapper
+        return inner
+
 class JsonResponse(HttpResponse):
     """
     HttpResponse descendant, which return response with ``application/json`` mimetype.
