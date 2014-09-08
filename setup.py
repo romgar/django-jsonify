5947dd6e4dd51ff671c51f0dcbeae579f8df09e4
Marius Grigaitis <m@mar.lt>
2014-09-08 23:49:42 +0300

    Version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ fhandler.close()
 
 setup(name='django-jsonify',
       packages=['jsonify', 'jsonify.templatetags'],
-      version='0.2.1',
+      version='0.3.0',
       description="Django additions for JSON",
       long_description=long_desc,
       author='Marius Grigaitis',
